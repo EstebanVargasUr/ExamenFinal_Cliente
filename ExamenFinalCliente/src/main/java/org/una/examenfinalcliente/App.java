0683284c30c8c0ce4906e9e7127469b35c2bfa73
Esteban Vargas Ureña <estebanvargasu@gmail.com>
2020-10-27 14:36:44 -0600

    Creación del proyecto con JavaFX

---
+++ ExamenFinalCliente/src/main/java/org/una/examenfinalcliente/App.java
@@ -0,0 +1,23 @@
+package org.una.examenfinalcliente;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {
+      
+    } 
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
